70edec7f0b9bf45829b93864b508573749a3509c
alessiaBer <alexdeyn@gmail.com>
2023-03-28 17:38:49 +0200

    [try] to change bg

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,12 +10,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            hello: 'Hello, I\'m Alessia',
+            hello: 'Hi, I\'m Alessia',
             message: 'I just started to use',
             divClassName: 'span',
             vueLogo: 'https://www.vectorlogo.zone/logos/vuejs/vuejs-icon.svg',
-            vueJs: 'Vue.js'
+            vueJs: 'Vue.js',
+            is_bg_changed: false
         }
     },
-    methods: {}
+    methods: {
+        changeBackground() {
+            console.log('ciao')
+            this.is_bg_changed = !this.is_bg_changed; 
+            console.log(this.is_bg_changed)
+        }
+    }
 }).mount('#app');
