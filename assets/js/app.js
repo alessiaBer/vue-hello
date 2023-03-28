238e7d9aa2540068e20a2e91be03bfc2ae652ce4
alessiaBer <alexdeyn@gmail.com>
2023-03-28 15:28:32 +0200

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/* Stampare a schermo un messaggio all’interno di un h1 
+utilizzando una proprietá nell'oggetto restituito dalla funzione data. 
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}).mount('#app')
